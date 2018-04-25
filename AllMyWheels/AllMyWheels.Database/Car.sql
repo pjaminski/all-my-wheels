3c092227bc6afff4b295ee939dae85e284691620
Piotr Jamiński <pjaminski@objectivity.co.uk>
2018-04-25 09:21:22 +0200

    US1_T1 - Create database project to keep SQL scripts

---
+++ AllMyWheels/AllMyWheels.Database/Car.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Car]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT newsequentialid(), 
+    [ProducerName] NVARCHAR(70) NOT NULL, 
+    [ModelName] NCHAR(70) NOT NULL, 
+    [ProducedFrom] INT NULL, 
+    [ProducedTo] INT NULL, 
+    [Description] NVARCHAR(500) NULL
+)
