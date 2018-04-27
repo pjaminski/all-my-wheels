c6342118591164afdb05ce0cef7f8a83c713becc
Piotr Jamiński <pjaminski@objectivity.co.uk>
2018-04-27 14:24:43 +0200

    US2_T1 - add SASS to the project / gulp tasks

---
+++ AllMyWheels/AllMyWheels/Gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function () {
+    gulp.src('./Content/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./Content/'));
+});
+
+gulp.task('default', function () {
+    gulp.watch('./Content/**/*.scss', ['styles']);
+});
